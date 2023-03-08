d284fbab2aa3d5323e9ca285242ee6b95c9122ee
Veronika-72 <119445104+Veronika-72@users.noreply.github.com>
2023-03-09 00:02:33 +0700

    Add files via upload

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -12,7 +12,59 @@ void NumberCounter (int number)
     if (number < 0) Console.Write($"{number} не натуральное число");
     if (number == 0) return;
     Console.Write("{0,4}", number);
-    NumberCounter (number - 1);
+    NumberCounter (number-1);
 }
 
 NumberCounter(number);
+
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30//
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+    int m = InputNumbers("Введите m: ");
+    int n = InputNumbers("Введите n: ");
+
+    int functionAkkerman = Ack(m, n);
+
+    Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+    int Ack(int m, int n)
+    {
+        if (m == 0) return n + 1;
+        else if (n == 0) return Ack(m - 1, 1);
+        else return Ack(m - 1, Ack(m, n - 1));
+    }
+
+    int InputNumbers(string input)
+    {
+        Console.Write(input);
+        int output = Convert.ToInt32(Console.ReadLine());
+        return output;
+    }
